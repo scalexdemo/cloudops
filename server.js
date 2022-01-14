762ba8e791cb71e6a46067071a6a50a793c9994a
scalexdemo <97677497+scalexdemo@users.noreply.github.com>
2022-01-14 23:24:31 +0530

    aa

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Hemant, Wellcome to Cloudops.\n');
+  res.send('Hello Hemant, wellcome to Cloudops project.\n');
 });
 
 app.listen(PORT);
